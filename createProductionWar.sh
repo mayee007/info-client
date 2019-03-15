f866118cb3a7995d5a58a616bf892ec1f084f6a4
Mahesh Mahadeva <yelekeri@gmail.com>
2019-03-15 03:16:55 +0000

    new files

---
+++ createProductionWar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo target file: $1
+
+cd target
+mkdir extract 
+cd extract 
+jar xvf ../*.war
+rm -rf ../*.war 
+cp WEB-INF/classes/application-prod.properties WEB-INF/classes/application.properties
+jar cvf ../$1 . 
